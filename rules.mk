46ee64118488de83b1155ea2645fcb60c55f56a8
TwoSune <146044579+TwoSune@users.noreply.github.com>
2024-10-14 09:40:29 -0700

    Add files via upload

---
+++ rules.mk
@@ -0,0 +1,5 @@
+RGBLIGHT_ENABLE     = yes
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+OLED_ENABLE         = no
+OLED_DRIVER         = SSD1306
+WS2812_DI_PIN = D3
