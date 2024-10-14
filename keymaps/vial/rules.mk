7fe2a3e66a8496c28c915bb697852acf72a3690d
TwoSune <146044579+TwoSune@users.noreply.github.com>
2024-10-14 09:48:37 -0700

    added files

---
+++ keymaps/vial/rules.mk
@@ -0,0 +1,9 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+
+RGBLIGHT_ENABLE     = yes
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+OLED_ENABLE         = no
+OLED_DRIVER         = SSD1306
+CONVERT_TO = HELIOS
+WS2812_DRIVER = vendor
