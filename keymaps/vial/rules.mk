31646b55bbdf3e7c081e0a251e0e03d89d1bd159
TwoSune <146044579+TwoSune@users.noreply.github.com>
2024-10-14 18:40:18 -0700

    h
    h

--- keymaps/vial/rules.mk
+++ keymaps/vial/rules.mk
@@ -5,5 +5,5 @@ RGBLIGHT_ENABLE     = yes
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 OLED_ENABLE         = no
 OLED_DRIVER         = SSD1306
-CONVERT_TO = HELIOS
+CONVERT_TO = helios
 WS2812_DRIVER = vendor
